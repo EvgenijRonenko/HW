58ab7c1a5ca7fd8b8f01d7c6a586b674c42d62a0
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-18 21:32:07 +0100

    1

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,25 +4,24 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any  //(filter: FilterType) => void -запоминай, как типизируется useState
-    deleteAffairCallback: any  //не забывай, что функция сюда приехала не пустой
+    data: AffairType[]
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
-        //пропс.setFilter('all')
+        props.setFilter('all')
     }
     const setHigh = () => {
-        // need to fix
+        props.setFilter('high')
     }
     const setMiddle = () => {
-        // need to fix
+        props.setFilter('middle')
     }
     const setLow = () => {
-        // need to fix
+        props.setFilter('low')
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
@@ -31,15 +30,9 @@ function Affairs(props: AffairsPropsType) {
     const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
 
 
-    // создаем переменную=мапим наши данные (affairs)=>{
-    // <вызываем компоненту <Affair в которую передаем глубже необходимые данные
-    // в том числе колбэк deleteAffairCallback
-    // />}
-    // получается, что мы мапим массив, но он не отрисовывается тут же, а погружается
-    // глубже в компоненту <Affair/> где произойдет отрисовка
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
@@ -77,12 +70,9 @@ function Affairs(props: AffairsPropsType) {
                     Low
                 </button>
             </div>
-            {/*Вот то что мы мапили на стр40 здесь используем. Можно было конечно прямо*/}
-            {/*здесь мапить и разводить болото, но мы решили работать по красивому и*/}
-            {/*все вынесли*/}
             <div className={s.affairs}>{mappedAffairs}</div>
         </div>
     )
 }
 
-export default Affairs
+export default Affairs;
