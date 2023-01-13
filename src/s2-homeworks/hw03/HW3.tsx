48be334df1484ccf319fc074bf0f1d17baa7edc5
EvgenijRonenko <evgenij.ronenko@gmail.com>
2023-01-13 21:06:30 +0100

    1

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { v1 } from 'uuid'
+import React, {useState} from 'react'
+import {v1} from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
@@ -19,30 +19,31 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string
+    name: string
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: (users: UserType[]) => void, users: UserType[]) => { // need to fix any
     const user = {
-        // need to fix
-        // нужно создать новый объект соответствующий типу UserType
-        // не забыть   сгенеририовать _id и вставить пришедший в функцию name
+        _id: v1(),
+        name: ''
     }
-    //засетайЮзеров([...старые users, и наш новый user])
+
+    setUsers([...users, user])
+
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([])
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => {
         pureAddUserCallback(name, setUsers, users)
     }
 
     return (
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #3</div>
-            {/*для автоматической проверки дз (не менять)*/}
+
 
             <div className={s2.hw}>
                 <GreetingContainer
